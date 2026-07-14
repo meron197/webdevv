718c43bd6e93afb2b8b47015f1cdb88add659c6f
meron zenebe <meronzenebe197@gmail.com>
2026-07-14 17:47:11 +0300

    Add files via upload

--- array2.cpp
+++ array2.cpp
@@ -1,30 +1,35 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
-int main(){
-int marks[5];
-int larg;
-int sum=0;
 
-for(int i =0;i<5;i++){
-	cout<<"enter mark"<<i+1<<endl;
-	cin>>marks[i];
-	sum=sum+marks[i];
-}	
-double ave = sum / 5.0;
-for(int u=0;u<marks[5];u++){
-	if( marks[u]>larg){
-		larg=marks[u];
-	}
-}
-cout<<"the smallest numbers are"<<endl;
-for(int l=1;l<5;l++){
-	if(marks[l]!=larg){
-		
-		cout<<marks[l]<<" ";
-	}
-}
-cout<<endl;
-cout<<"the sum is "<<sum<<"the average is "<<ave<<"the laregest number is "<<larg<<endl;
+int main() {
+    int marks[5];
+    int sum = 0;
 
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter mark " << i + 1 << ": ";
+        cin >> marks[i];
+        sum += marks[i];
+    }
 
+    double ave = sum / 5.0;
+
+    int larg = marks[0];   
+
+    for (int u = 1; u < 5; u++) {
+        if (marks[u] > larg) {
+            larg = marks[u];
+        }
+    }
+
+    cout << "Numbers except the largest are:" << endl;
+    for (int l = 0; l < 5; l++) {
+        if (marks[l] != larg) {
+            cout << marks[l] << " ";
+        }
+    }
+
+    cout << endl;
+    cout << "The sum is " << sum << "The average is " << ave << "The largest number is " << larg << endl;
+
+    
 }
